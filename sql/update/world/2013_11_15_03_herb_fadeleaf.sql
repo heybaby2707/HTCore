4f3fe98e89309aa678ae6727bfb9b1d6e7a14def
heybaby2707 <heybaby2707@gmail.com>
2013-11-15 07:30:47 +0700

    DB: Fix Herb Fadeleaf

---
+++ sql/update/world/2013_11_15_03_herb_fadeleaf.sql
@@ -0,0 +1,6 @@
+-- Loot Fadeleaf
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=1731);
+INSERT INTO `gameobject_loot_template` VALUES 
+(1731, 774, 3, 1, 0, 1, 1),
+(1731, 818, 3, 1, 0, 1, 1),
+(1731, 1210, 3, 1, 0, 1, 1);
