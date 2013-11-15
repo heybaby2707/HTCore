fdbb772c4ff84e62fb21cc60c6b65db75532e36b
heybaby2707 <heybaby2707@gmail.com>
2013-11-15 08:00:05 +0700

    DB: Change Mat Rate

---
+++ sql/update/world/2013_11_15_04_mat_rate.sql
@@ -0,0 +1,134 @@
+-- Mine
+-- Elementium
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=28492);
+INSERT INTO `gameobject_loot_template` VALUES 
+(28492, 52177, 0.8, 1, 0, 1, 1),
+(28492, 52178, 0.8, 1, 0, 1, 1),
+(28492, 52179, 0.8, 1, 0, 1, 1),
+(28492, 52180, 0.8, 1, 0, 1, 1),
+(28492, 52181, 0.8, 1, 0, 1, 1),
+(28492, 52182, 0.8, 1, 0, 1, 1),
+(28492, 52185, 100, 1, 0, 1, 2),
+(28492, 52325, 10, 1, 0, 1, 2),
+(28492, 52326, 10, 1, 0, 1, 2),
+(28492, 52327, 10, 1, 0, 1, 2),
+(28492, 52328, 2, 1, 0, 1, 2),
+(28492, 67282, 0.05, 1, 0, 1, 1);
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=28493);
+INSERT INTO `gameobject_loot_template` VALUES 
+(28493, 52177, 0.8, 1, 0, 1, 1),
+(28493, 52178, 0.8, 1, 0, 1, 1),
+(28493, 52179, 0.8, 1, 0, 1, 1),
+(28493, 52180, 0.8, 1, 0, 1, 1),
+(28493, 52181, 0.8, 1, 0, 1, 1),
+(28493, 52182, 0.9, 1, 0, 1, 1),
+(28493, 52185, 100, 1, 0, 2, 3),
+(28493, 52325, 20, 1, 0, 1, 2),
+(28493, 52326, 19, 1, 0, 1, 2),
+(28493, 52327, 20, 1, 0, 1, 2),
+(28493, 52328, 4, 1, 0, 1, 2),
+(28493, 67282, 0.1, 1, 0, 1, 1);
+-- Obsidium
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=28490);
+INSERT INTO `gameobject_loot_template` VALUES 
+(28490, 52177, 0.8, 1, 0, 1, 1),
+(28490, 52178, 0.8, 1, 0, 1, 1),
+(28490, 52179, 0.8, 1, 0, 1, 1),
+(28490, 52180, 0.8, 1, 0, 1, 1),
+(28490, 52181, 0.8, 1, 0, 1, 1),
+(28490, 52182, 0.9, 1, 0, 1, 1),
+(28490, 52327, 10, 1, 0, 1, 2),
+(28490, 52328, 1.8, 1, 0, 1, 2),
+(28490, 53038, 100, 1, 0, 1, 2);
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=28495);
+INSERT INTO `gameobject_loot_template` VALUES 
+(28495, 52177, 0.8, 1, 0, 1, 1),
+(28495, 52178, 0.7, 1, 0, 1, 1),
+(28495, 52179, 0.9, 1, 0, 1, 1),
+(28495, 52180, 0.9, 1, 0, 1, 1),
+(28495, 52181, 0.9, 1, 0, 1, 1),
+(28495, 52182, 0.8, 1, 0, 1, 1),
+(28495, 52327, 20, 1, 0, 3, 4),
+(28495, 52328, 4, 1, 0, 1, 2),
+(28495, 53038, 100, 1, 0, 2, 3);
+-- Titanium
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=24157);
+INSERT INTO `gameobject_loot_template` VALUES 
+(24157, 12995, 8, 1, 0, -12995, 1),
+(24157, 12996, 24, 1, 0, -12996, 1),
+(24157, 36910, 100, 1, 0, 1, 2),
+(24157, 37700, 50, 1, 0, 1, 8),
+(24157, 37701, 50, 1, 0, 1, 8),
+(24157, 37702, 50, 1, 0, 1, 8),
+(24157, 37705, 50, 1, 0, 1, 8);
+-- Saronite
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=24155);
+INSERT INTO `gameobject_loot_template` VALUES 
+(24155, 12995, 1, 1, 0, -12995, 1),
+(24155, 12996, 2, 1, 0, -12996, 1),
+(24155, 36912, 100, 1, 0, 1, 2),
+(24155, 37701, 25, 1, 0, 1, 8),
+(24155, 37703, 25, 1, 0, 1, 8);
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=27244);
+INSERT INTO `gameobject_loot_template` VALUES 
+(27244, 35624, 100, 1, 0, 2, 4),
+(27244, 35627, 100, 1, 0, 1, 3),
+(27244, 36912, 100, 1, 0, 18, 23),
+(27244, 36917, 100, 1, 0, 1, 1),
+(27244, 36918, 100, 1, 0, 1, 1),
+(27244, 36920, 100, 1, 0, 2, 5),
+(27244, 36921, 100, 1, 0, 1, 1),
+(27244, 36923, 100, 1, 0, 1, 1),
+(27244, 36924, 100, 1, 0, 1, 1),
+(27244, 36926, 100, 1, 0, 1, 4),
+(27244, 36927, 100, 1, 0, 1, 1),
+(27244, 36929, 100, 1, 0, 1, 1),
+(27244, 36930, 100, 1, 0, 1, 1),
+(27244, 36932, 100, 1, 0, 1, 1),
+(27244, 36933, 100, 1, 0, 1, 1);
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=24156);
+INSERT INTO `gameobject_loot_template` VALUES 
+(24156, 12993, 2, 1, 0, -12993, 1),
+(24156, 12994, 12, 1, 0, -12994, 1),
+(24156, 36912, 100, 1, 0, 2, 4),
+(24156, 37701, 25, 1, 0, 1, 14),
+(24156, 37703, 25, 1, 0, 1, 14);
+
+
+-- Herb
+-- Twilight Jasmine
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=202751);
+INSERT INTO `gameobject_loot_template` VALUES 
+(202751, 52329, 29, 1, 0, 1, 2),
+(202751, 52987, 100, 1, 0, 2, 3),
+(202751, 63122, 10, 1, 0, 1, 1);
+-- Azshara
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=28523);
+INSERT INTO `gameobject_loot_template` VALUES 
+(28523, 52329, 29, 1, 0, 1, 2),
+(28523, 52985, 100, 1, 0, 1, 3),
+(28523, 63122, 10, 1, 0, 1, 1);
+-- Whiptail
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=28526);
+INSERT INTO `gameobject_loot_template` VALUES 
+(28526, 52329, 29, 1, 0, 1, 2),
+(28526, 52988, 100, 1, 0, 1, 3),
+(28526, 63122, 10, 1, 0, 1, 1);
+-- Heartblossom
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=28524);
+INSERT INTO `gameobject_loot_template` VALUES 
+(28524, 52329, 28, 1, 0, 1, 2),
+(28524, 52986, 100, 1, 0, 1, 3),
+(28524, 63122, 10, 1, 0, 1, 1);
+-- Stormvine
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=28522);
+INSERT INTO `gameobject_loot_template` VALUES 
+(28522, 52329, 29, 1, 0, 1, 2),
+(28522, 52984, 100, 1, 0, 1, 3),
+(28522, 63122, 10, 1, 0, 1, 1);
+-- 
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=28521);
+INSERT INTO `gameobject_loot_template` VALUES 
+(28521, 52329, 29, 1, 0, 1, 2),
+(28521, 52983, 100, 1, 0, 1, 3),
+(28521, 63122, 10, 1, 0, 1, 1);
