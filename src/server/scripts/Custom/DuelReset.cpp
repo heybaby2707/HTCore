19867e62999ade21d3756cbb8d8a25cce4362686
heybaby2707 <heybaby2707@gmail.com>
2013-11-28 04:49:19 +0700

    Core/Script: Fix custom DuelReset

--- src/server/scripts/Custom/DuelReset.cpp
+++ src/server/scripts/Custom/DuelReset.cpp
@@ -15,16 +15,18 @@ class Reset_OnDuelEnd : public PlayerScript
 		// reset cooldowns (in order) Elwyn, Durotar, Tirisfal, Tendrasil, Dun Morogh, Gurubashi Arena, Exodar, Mulgore
 		if (winner->GetZoneId() == 12 || winner->GetZoneId() == 14 || winner->GetZoneId() == 85 || winner->GetZoneId() == 141 || winner->GetZoneId() == 1 || winner->GetZoneId() == 5287 || winner->GetZoneId() == 3524 || winner->GetZoneId() == 215)
 		{
+			// Winner
 			winner->SetHealth(winner->GetMaxHealth());
 			winner->SetPower(POWER_MANA, winner->GetMaxPower(POWER_MANA));
 			winner->RemoveArenaSpellCooldowns();
 			winner->GetSession()->SendNotification("Ban nhan duoc 50 Silver tu He Thong vi chien thang trong tran Duel! Hay giu vung phong do!");
 			winner->SetMoney(winner->GetMoney() + 5000); // 50 sil
+			// Loser
 			loser->RemoveArenaSpellCooldowns();
 			loser->SetHealth(loser->GetMaxHealth());
 			loser->SetPower(POWER_MANA, loser->GetMaxPower(POWER_MANA));
 			loser->GetSession()->SendNotification("Dung buon vi da thua! Hay tiep tuc co gang hon nua! Ban nhan duoc 25 Silver tu He Thong.");
-			loser->SetMoney(winner->GetMoney() + 2500); // 25 sil
+			loser->SetMoney(loser->GetMoney() + 2500); // 25 sil
 		}
 	}
 };
